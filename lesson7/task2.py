2d7d7b64df16bbfe59d30c6678df906048727dc0
Vlad Chekunov <gwellir@gmail.com>
2019-12-26 14:29:38 +0300

    optimize checks for trivial recursion steps

--- lesson7/task2.py
+++ lesson7/task2.py
@@ -9,9 +9,7 @@ MAX_ITEM = 50
 
 
 def merge_sort(arr, start, end):
-    if end - start == 1:
-        return
-    elif end - start == 2:
+    if end - start < 3:
         if arr[start] > arr[end - 1]:
             arr[start], arr[end - 1] = arr[end - 1], arr[start]
         return
